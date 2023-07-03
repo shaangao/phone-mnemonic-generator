3ba320761f173abb871250bf9dbc745d89de6085
shaangao <shangaocog@gmail.com>
2023-07-02 19:18:58 -0500

    init (move files)

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,9 +1,3 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//    }
-//}
-
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
